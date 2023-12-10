bbedddab17168be2b3d85db86c77d09af0a52a14
Shahnawaz <shahnawaz@ranoliaventures.com>
2023-12-10 12:57:31 +0530

    updates

--- app/Http/Controllers/Company/DashboardController.php
+++ app/Http/Controllers/Company/DashboardController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Company;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $totalEmployee = Employee::where('is_deleted','!=','1')->count();
+        $companyId = Auth::guard('web')->user()->company_id;
+        $totalEmployee = Employee::where('is_deleted','!=','1')
+                            ->where('company_id',$companyId)
+                            ->count();
         return view('dashboard',[
             'totalEmployee'=>$totalEmployee
         ]);
